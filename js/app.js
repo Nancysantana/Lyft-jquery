d5b0894eeeea4dc124190c868bd106cd9089f228
Nancysantana <yaretsita.27@gmail.com>
2018-01-23 11:13:51 -0600

    jquery

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
-$(document).ready(function() {
-  $('select').material_select();
+
+function addContact(e) {
+  e.preventDefault();
+    var phone = $phoneInput.val();
+}
+  $phone=$('#phone-input');
+   $phone.keyup (function () {
+     $next=$('#activ-next');
+     var $phoneVal= (($phone).val().length);
+       if($phoneVal == 10) {
+         $next.removeAttr('disabled');
+         $next.addClass('able');
+       }
+       else {
+         $next.attr('disabled', true);
+         $next.removeClass('able');
+       }
   });
+
+
+  function getRandomInt(min, max) {
+ return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+
+
+
+$(document).ready(function();
