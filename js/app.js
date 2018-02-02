abc94ef37b7745192b95f315eb42b130e094493a
Nancysantana <yaretsita.27@gmail.com>
2018-02-02 05:55:26 -0600

    maquetado lyft

--- js/app.js
+++ js/app.js
@@ -1,28 +1,32 @@
 
-function addContact(e) {
-  e.preventDefault();
-    var phone = $phoneInput.val();
-}
-  $phone=$('#phone-input');
-   $phone.keyup (function () {
-     $next=$('#activ-next');
-     var $phoneVal= (($phone).val().length);
-       if($phoneVal == 10) {
-         $next.removeAttr('disabled');
-         $next.addClass('able');
-       }
-       else {
-         $next.attr('disabled', true);
-         $next.removeClass('able');
-       }
-  });
 
 
-  function getRandomInt(min, max) {
- return Math.floor(Math.random() * (max - min)) + min;
-}
 
 
+
+//
+// function addContact(e) {
+//   e.preventDefault();
+//     var phone = $phoneInput.val();
+// }
+//   $phone=$('#phone-input');
+//    $phone.keyup (function () {
+//      $next=$('#activ-next');
+//      var $phoneVal= (($phone).val().length);
+//        if($phoneVal == 10) {
+//          $next.removeAttr('disabled');
+//          $next.addClass('able');
+//        }
+//        else {
+//          $next.attr('disabled', true);
+//          $next.removeClass('able');
+//        }
+//   });
+//
+//
+//   function getRandomInt(min, max) {
+//  return Math.floor(Math.random() * (max - min)) + min;
+// }
 
 
 
